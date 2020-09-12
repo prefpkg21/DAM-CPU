91c7d9eb170bcbd87120eda6412ce100ac3f6afb
Blake <prefpkg21@users.noreply.github.com>
2020-09-12 00:17:34 -0400

    Revert to three floats

--- damm_msgs/Servos.h
+++ damm_msgs/Servos.h
@@ -18,8 +18,6 @@ namespace damm_msgs
       _dxl2_type dxl2;
       typedef float _dxl3_type;
       _dxl3_type dxl3;
-      typedef float _dxl4_type;
-      _dxl4_type dxl4;
       typedef float _linear_type;
       _linear_type linear;
 
@@ -27,7 +25,6 @@ namespace damm_msgs
       dxl1(0),
       dxl2(0),
       dxl3(0),
-      dxl4(0),
       linear(0)
     {
     }
@@ -35,27 +32,101 @@ namespace damm_msgs
     virtual int serialize(unsigned char *outbuffer) const
     {
       int offset = 0;
-      offset += serializeAvrFloat64(outbuffer + offset, this->dxl1);
-      offset += serializeAvrFloat64(outbuffer + offset, this->dxl2);
-      offset += serializeAvrFloat64(outbuffer + offset, this->dxl3);
-      offset += serializeAvrFloat64(outbuffer + offset, this->dxl4);
-      offset += serializeAvrFloat64(outbuffer + offset, this->linear);
+      union {
+        float real;
+        uint32_t base;
+      } u_dxl1;
+      u_dxl1.real = this->dxl1;
+      *(outbuffer + offset + 0) = (u_dxl1.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_dxl1.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_dxl1.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_dxl1.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->dxl1);
+      union {
+        float real;
+        uint32_t base;
+      } u_dxl2;
+      u_dxl2.real = this->dxl2;
+      *(outbuffer + offset + 0) = (u_dxl2.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_dxl2.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_dxl2.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_dxl2.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->dxl2);
+      union {
+        float real;
+        uint32_t base;
+      } u_dxl3;
+      u_dxl3.real = this->dxl3;
+      *(outbuffer + offset + 0) = (u_dxl3.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_dxl3.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_dxl3.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_dxl3.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->dxl3);
+      union {
+        float real;
+        uint32_t base;
+      } u_linear;
+      u_linear.real = this->linear;
+      *(outbuffer + offset + 0) = (u_linear.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_linear.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_linear.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_linear.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->linear);
       return offset;
     }
 
     virtual int deserialize(unsigned char *inbuffer)
     {
       int offset = 0;
-      offset += deserializeAvrFloat64(inbuffer + offset, &(this->dxl1));
-      offset += deserializeAvrFloat64(inbuffer + offset, &(this->dxl2));
-      offset += deserializeAvrFloat64(inbuffer + offset, &(this->dxl3));
-      offset += deserializeAvrFloat64(inbuffer + offset, &(this->dxl4));
-      offset += deserializeAvrFloat64(inbuffer + offset, &(this->linear));
+      union {
+        float real;
+        uint32_t base;
+      } u_dxl1;
+      u_dxl1.base = 0;
+      u_dxl1.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_dxl1.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_dxl1.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_dxl1.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->dxl1 = u_dxl1.real;
+      offset += sizeof(this->dxl1);
+      union {
+        float real;
+        uint32_t base;
+      } u_dxl2;
+      u_dxl2.base = 0;
+      u_dxl2.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_dxl2.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_dxl2.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_dxl2.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->dxl2 = u_dxl2.real;
+      offset += sizeof(this->dxl2);
+      union {
+        float real;
+        uint32_t base;
+      } u_dxl3;
+      u_dxl3.base = 0;
+      u_dxl3.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_dxl3.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_dxl3.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_dxl3.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->dxl3 = u_dxl3.real;
+      offset += sizeof(this->dxl3);
+      union {
+        float real;
+        uint32_t base;
+      } u_linear;
+      u_linear.base = 0;
+      u_linear.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_linear.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_linear.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_linear.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->linear = u_linear.real;
+      offset += sizeof(this->linear);
      return offset;
     }
 
     const char * getType(){ return "damm_msgs/Servos"; };
-    const char * getMD5(){ return "880708faca70afa51821f096d5e1f01d"; };
+    const char * getMD5(){ return "407cdb1ad6178d9cbff717a19cc80744"; };
 
   };
 
